1086eea537c0bf643260aa97fae3044dd44ecace
BillKaterinis <77237632+BillKtr11@users.noreply.github.com>
2025-07-07 13:02:56 +0300

    Add files via upload

---
+++ lexer.h
@@ -0,0 +1,32 @@
+// MYHTML
+#define MYHTML 1
+#define MYHTML_CLOSE 2
+#define HEAD 3
+#define HEAD_CLOSE 4
+#define BODY 5
+#define BODY_CLOSE 6
+
+// HEAD
+#define TITLE 7
+#define TITLE_CLOSE 8
+#define META 9
+
+// BODY
+#define P 10
+#define P_CLOSE 11
+#define A 12
+#define A_CLOSE 13
+#define IMG 14
+#define IMG_CLOSE 15
+#define FORM 16
+#define FORM_CLOSE 17
+#define INPUT 18
+#define LABEL 19
+#define LABEL_CLOSE 20
+#define DIV 21
+#define DIV 22
+
+
+// EOF
+#define T_EOF 0
+
